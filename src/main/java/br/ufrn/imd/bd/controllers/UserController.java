6a47471efa77cfa8e96ed580e511d05982f00cb2
Pablo Paiva <pablodeyvid11@gmail.com>
2024-06-23 17:01:08 -0300

    some changes

--- src/main/java/br/ufrn/imd/bd/controllers/UserController.java
+++ src/main/java/br/ufrn/imd/bd/controllers/UserController.java
@@ -1,6 +1,7 @@
 package br.ufrn.imd.bd.controllers;
 
 import java.net.URI;
+import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -25,97 +26,104 @@ import br.ufrn.imd.bd.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    private UserService service;
+	@Autowired
+	private UserService service;
 
-    @Operation(summary = "Registrar um novo usuário")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "201", description = "Usuário registrado com sucesso"),
-        @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> signon(@RequestBody(required = true) @Valid UserDTO signonRecord) {
-        try {
-            User userCreated = service.signon(signonRecord);
-            URI uri = URI.create("/user/" + userCreated.getId());
-            return ResponseEntity.created(uri).build();
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
-                new ApiErrorDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "/user", "Error on register user"));
-        }
-    }
+	@Operation(summary = "Registrar um novo usuário")
+	@ApiResponses(value = { @ApiResponse(responseCode = "201", description = "Usuário registrado com sucesso"),
+			@ApiResponse(responseCode = "500", description = "Erro interno do servidor") })
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> signon(@RequestBody(required = true) @Valid UserDTO signonRecord,
+			HttpServletRequest request) {
+		try {
+			User userCreated = service.signon(signonRecord);
+			URI uri = URI.create("/user/" + userCreated.getId());
+			return ResponseEntity.created(uri).build();
+		} catch (SQLException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new ApiErrorDTO(HttpStatus.BAD_REQUEST.value(), request.getRequestURI(), e.getMessage()));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ApiErrorDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "/user", "Error on register user"));
+		}
+	}
 
-    @Operation(summary = "Login de um usuário")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Login realizado com sucesso"),
-        @ApiResponse(responseCode = "400", description = "Login incorreto")
-    })
-    @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> signin(@RequestBody(required = true) @Valid SigninDTO signonRecord) {
-        try {
-            RecoveryJWTDTO token = service.login(signonRecord);
-            return ResponseEntity.ok(token);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                new ApiErrorDTO(HttpStatus.BAD_REQUEST.value(), "/user/login", "Login incorreto"));
-        }
-    }
+	@Operation(summary = "Login de um usuário")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Login realizado com sucesso"),
+			@ApiResponse(responseCode = "400", description = "Login incorreto") })
+	@PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> signin(@RequestBody(required = true) @Valid SigninDTO signonRecord,
+			HttpServletRequest request) {
+		try {
+			RecoveryJWTDTO token = service.login(signonRecord);
+			return ResponseEntity.ok(token);
+		} catch (SQLException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new ApiErrorDTO(HttpStatus.BAD_REQUEST.value(), request.getRequestURI(), e.getMessage()));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new ApiErrorDTO(HttpStatus.BAD_REQUEST.value(), "/user/login", "Login incorreto"));
+		}
+	}
 
-    @Operation(summary = "Atualizar um usuário")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Usuário atualizado com sucesso"),
-        @ApiResponse(responseCode = "400", description = "Dados inválidos"),
-        @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
-    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> update(@RequestBody(required = true) @Valid UserDTO signonRecord,
-                                    @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) {
-        try {
-            User u = service.findUserFromToken(authorizationHeader);
-            if (!u.getEmail().equals(signonRecord.email())) {
-                return ResponseEntity.badRequest().body(
-                    new ApiErrorDTO(HttpStatus.BAD_REQUEST.value(), "/user", "You can't edit others user"));
-            }
-            service.update(signonRecord);
-            return ResponseEntity.ok(service.findByEmail(signonRecord.email()));
-        } catch (IllegalStateException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                new ApiErrorDTO(HttpStatus.BAD_REQUEST.value(), "/user", e.getMessage()));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
-                new ApiErrorDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "/user", "Error on update user"));
-        }
-    }
+	@Operation(summary = "Atualizar um usuário")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Usuário atualizado com sucesso"),
+			@ApiResponse(responseCode = "400", description = "Dados inválidos"),
+			@ApiResponse(responseCode = "500", description = "Erro interno do servidor") })
+	@PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> update(@RequestBody(required = true) @Valid UserDTO signonRecord,
+			@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, HttpServletRequest request) {
+		try {
+			User u = service.findUserFromToken(authorizationHeader);
+			if (!u.getEmail().equals(signonRecord.email())) {
+				return ResponseEntity.badRequest()
+						.body(new ApiErrorDTO(HttpStatus.BAD_REQUEST.value(), "/user", "You can't edit others user"));
+			}
+			service.update(signonRecord);
+			return ResponseEntity.ok(service.findByEmail(signonRecord.email()));
+		} catch (SQLException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new ApiErrorDTO(HttpStatus.BAD_REQUEST.value(), request.getRequestURI(), e.getMessage()));
+		} catch (IllegalStateException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new ApiErrorDTO(HttpStatus.BAD_REQUEST.value(), "/user", e.getMessage()));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ApiErrorDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "/user", "Error on update user"));
+		}
+	}
 
-    @Operation(summary = "Deletar um usuário")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "204", description = "Usuário deletado com sucesso"),
-        @ApiResponse(responseCode = "400", description = "Dados inválidos"),
-        @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
-    })
-    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> delete(@PathVariable(name = "id") Long id,
-                                    @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) {
-        try {
-            User u = service.findUserFromToken(authorizationHeader);
-            if (!u.getId().equals(id)) {
-                return ResponseEntity.badRequest().body(
-                    new ApiErrorDTO(HttpStatus.BAD_REQUEST.value(), "/user/" + id, "You can't delete others user"));
-            }
-            service.deleteById(id);
-            return ResponseEntity.noContent().build();
-        } catch (IllegalStateException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
-                new ApiErrorDTO(HttpStatus.BAD_REQUEST.value(), "/user/" + id, e.getMessage()));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
-                new ApiErrorDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "/user/" + id, "Error on delete user"));
-        }
-    }
+	@Operation(summary = "Deletar um usuário")
+	@ApiResponses(value = { @ApiResponse(responseCode = "204", description = "Usuário deletado com sucesso"),
+			@ApiResponse(responseCode = "400", description = "Dados inválidos"),
+			@ApiResponse(responseCode = "500", description = "Erro interno do servidor") })
+	@DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> delete(@PathVariable(name = "id") Long id,
+			@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, HttpServletRequest request) {
+		try {
+			User u = service.findUserFromToken(authorizationHeader);
+			if (!u.getId().equals(id)) {
+				return ResponseEntity.badRequest().body(
+						new ApiErrorDTO(HttpStatus.BAD_REQUEST.value(), "/user/" + id, "You can't delete others user"));
+			}
+			service.deleteById(id);
+			return ResponseEntity.noContent().build();
+		} catch (SQLException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new ApiErrorDTO(HttpStatus.BAD_REQUEST.value(), request.getRequestURI(), e.getMessage()));
+		} catch (IllegalStateException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(new ApiErrorDTO(HttpStatus.BAD_REQUEST.value(), "/user/" + id, e.getMessage()));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+					new ApiErrorDTO(HttpStatus.INTERNAL_SERVER_ERROR.value(), "/user/" + id, "Error on delete user"));
+		}
+	}
 }
